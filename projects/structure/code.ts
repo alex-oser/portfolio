96dfb960f5ec21a8e0688a031d3bccd5d228bcaa
Alex Oser <aoser9552@gmail.com>
2021-07-18 20:17:49 -0500

    Update structure info

---
+++ projects/structure/code.ts
@@ -0,0 +1,16 @@
+export const tree = `# https://github.com/structure-codes/structure
+# react
+# firebase
+├── api/
+│	└── src/
+├── public/
+└── src/
+	├── AboutView/
+	├── App/
+	│	└── Header/
+	├── HomeView/
+	│	├── CodePanel/
+	│	├── Dropdown/
+	│	├── ModelPanel/
+	│	└── SettingsPanel/
+	└── WelcomeModal/`;
